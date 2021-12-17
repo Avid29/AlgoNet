3d9d7f6d07eeac63d97209ffa652f78885b58d30
Adam Dernis <avid29@live.com>
2021-12-17 06:27:39 -0500

    Double and Vector2 tests

---
+++ tests/AlgoNet.Tests.Common/Tests/DoubleTests.cs
@@ -0,0 +1,20 @@
+﻿// Adam Dernis © 2021
+
+namespace AlgoNet.Tests.Tests
+{
+    public static class DoubleTests
+    {
+        public static Test<double> DoubleTest1 = new Test<double>(
+            "Double test 1",
+            new double[]
+            {
+                0,
+                1,
+                8,
+                10,
+                12,
+                22,
+                24,
+            }, 5, 3);
+    }
+}
