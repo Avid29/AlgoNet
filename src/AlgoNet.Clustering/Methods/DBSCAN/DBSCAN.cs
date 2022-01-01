fdadb6a953ff48e0a2ae7199d7715b450cc3b859
Adam Dernis <avid29@live.com>
2022-01-01 16:34:51 +0000

    Added Basic DBSCAN objects

---
+++ src/AlgoNet.Clustering/Methods/DBSCAN/DBSCAN.cs
@@ -0,0 +1,11 @@
+﻿// Adam Dernis © 2022
+
+namespace AlgoNet.Clustering
+{
+    /// <summary>
+    /// A static class containing DBSCAN methods.
+    /// </summary>
+    public static class DBSCAN
+    {
+    }
+}
