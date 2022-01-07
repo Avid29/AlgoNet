de10cbf9cf818216c94c2cc5a46e02cf96172499
Adam Dernis <avid29@live.com>
2022-01-07 00:36:50 -0500

    InsertionSort tests

---
+++ tests/AlgoNet.Tests.Sorting/Methods/InsertionSortTests.cs
@@ -0,0 +1,36 @@
+﻿// Adam Dernis © 2022
+
+using AlgoNet.Tests.Sorting.DataSets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IS = AlgoNet.Sorting.InsertionSort;
+
+namespace AlgoNet.Tests.Sorting.Methods
+{
+    [TestClass]
+    public class InsertionSortTests
+    {
+        private static void RunTest(int[] data)
+        {
+            IS.Sort(data);
+            Common.VerifySorted(data);
+        }
+
+        [TestMethod]
+        public void PresortedTests()
+        {
+            foreach (var set in PresortedSets.All)
+            {
+                RunTest(set);
+            }
+        }
+
+        [TestMethod]
+        public void ReverseOrderTests()
+        {
+            foreach (var set in ReverseOrderSets.All)
+            {
+                RunTest(set);
+            }
+        }
+    }
+}
