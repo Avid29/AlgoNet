06cb7197a7bb650706c6ec1d4dbd9c9873fecda7
Adam Dernis <avid29@live.com>
2022-01-06 23:58:55 -0500

    KMeans algorithm implementation

---
+++ src/AlgoNet.Clustering/Methods/KMeans/IO/KMeansConfig.cs
@@ -0,0 +1,35 @@
+﻿// Adam Dernis © 2022
+
+using System;
+
+namespace AlgoNet.Clustering
+{
+    /// <summary>
+    /// A struct containing configuration info for KMeans Cluster to run.
+    /// </summary>
+    /// <typeparam name="T">The type of data in the cluster.</typeparam>
+    /// <typeparam name="TShape">A shape to describe to provide comparison methods for <see cref="T"/>.</typeparam>
+    public class KMeansConfig<T, TShape>
+        where T : unmanaged
+        where TShape : struct, IGeometricPoint<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KMeansConfig{T, TShape}"/> struct.
+        /// </summary>
+        /// <param name="clustCount">The number of clusters to create with KMeans. Must be greater than or equal to two (2).</param>
+        public KMeansConfig(int clustCount)
+        {
+            if (clustCount < 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(clustCount), clustCount, $"{nameof(clustCount)} must be greater than or equal to two (2).");
+            }
+
+            ClusterCount = clustCount;
+        }
+
+        /// <summary>
+        /// Gets the number of clusters to create with KMeans.
+        /// </summary>
+        public int ClusterCount { get; }
+    }
+}
