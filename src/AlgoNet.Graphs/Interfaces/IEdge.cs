413da357d097c9627f8e814ed16220ff16e10c5c
Adam Dernis <avid29@live.com>
2022-02-08 13:16:05 -0500

    WIP Added node and edge interfaces

---
+++ src/AlgoNet.Graphs/Interfaces/IEdge.cs
@@ -0,0 +1,9 @@
+﻿// Adam Dernis © 2022
+
+namespace AlgoNet.Graphs.Interfaces
+{
+    public interface IEdge
+    {
+        double Double { get; }
+    }
+}
---
+++ src/AlgoNet.Graphs/Interfaces/INode.cs
@@ -0,0 +1,11 @@
+﻿// Adam Dernis © 2022
+
+using System.Collections.Generic;
+
+namespace AlgoNet.Graphs.Interfaces
+{
+    public interface INode
+    {
+        List<IEdge> Edges { get; }
+    }
+}
