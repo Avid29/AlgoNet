69b872768f2dc452740ae6e84ecb3c7428ab84b7
Adam Dernis <avid29@live.com>
2022-02-09 09:37:33 -0500

    Added Benchmarks project

---
+++ bench/AlgoNet.Benchmarks.Sorting/Benchmarks/SortingBenchmarks.cs
@@ -0,0 +1,45 @@
+﻿// Adam Dernis © 2022
+
+using AlgoNet.Sorting;
+using AlgoNet.Sorting.Shuffle;
+using BenchmarkDotNet.Attributes;
+
+namespace AlgoNet.Benchmarks.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class SortingBenchmarks
+    {
+        private int[] _array = new int[1];
+
+        [Params(10, 100, 1_000, 10_000)]
+        public int Numbers;
+
+        [Params(true, false)]
+        public bool Randomized;
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _array = Enumerable.Range(0, Numbers).ToArray();
+            if (Randomized) FisherYates.Shuffle(_array);
+        }
+
+        [Benchmark]
+        public void AlgoNet_QuickSort()
+        {
+            QuickSort.Sort(_array);
+        }
+
+        [Benchmark]
+        public void AlgoNet_MergeSort()
+        {
+            MergeSort.Sort(_array);
+        }
+
+        [Benchmark]
+        public void Array_Sort()
+        {
+            Array.Sort(_array);
+        }
+    }
+}
