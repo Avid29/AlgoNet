6e344da3bdac80d7e440805029f975927388b39f
Adam Dernis <avid29@live.com>
2022-02-08 23:58:43 -0500

    Added larger DataSets to improve speed comparisons

--- tests/AlgoNet.Tests.Sorting/DataSets/PresortedSets.cs
+++ tests/AlgoNet.Tests.Sorting/DataSets/PresortedSets.cs
@@ -14,16 +14,30 @@ namespace AlgoNet.Tests.Sorting.DataSets
                 new DimensionSpecs(0, 99, 100, new LinearEase()),
             });
 
-        public static int[] DuplicateInteger10 = GradientGenerator.Generate<int, IntegerGradientShape>(
+        public static int[] UniqueInteger1000 = GradientGenerator.Generate<int, IntegerGradientShape>(
+            new DimensionSpecs[]
+            {
+                new DimensionSpecs(0, 999, 1000, new LinearEase()),
+            });
+
+        public static int[] DuplicateInteger100 = GradientGenerator.Generate<int, IntegerGradientShape>(
             new DimensionSpecs[]
             {
                 new DimensionSpecs(0, 24, 100, new LinearEase()),
             });
 
+        public static int[] DuplicateInteger1000 = GradientGenerator.Generate<int, IntegerGradientShape>(
+            new DimensionSpecs[]
+            {
+                new DimensionSpecs(0, 249, 1000, new LinearEase()),
+            });
+
         public static int[][] All = new int[][]
         {
             UniqueInteger100,
-            DuplicateInteger10,
+            UniqueInteger1000,
+            DuplicateInteger100,
+            DuplicateInteger1000,
         };
     }
 }
