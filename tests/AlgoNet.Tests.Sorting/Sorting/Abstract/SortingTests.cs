317f64bfaa3b600d917d8dd03b5b8cffe475f815
Adam Dernis <avid29@live.com>
2022-02-10 12:23:19 -0500

    Abstracted sorting tests

---
+++ tests/AlgoNet.Tests.Sorting/Sorting/Abstract/SortingTests.cs
@@ -0,0 +1,39 @@
+﻿// Adam Dernis © 2022
+
+using AlgoNet.Tests.Sorting.DataSets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoNet.Tests.Sorting
+{
+    public abstract class SortingTests
+    {
+        protected abstract void RunTest(int[] data);
+
+        [TestMethod]
+        public void PresortedTests()
+        {
+            foreach (var set in PresortedSets.All)
+            {
+                RunTest(set);
+            }
+        }
+
+        [TestMethod]
+        public void ReverseOrderTests()
+        {
+            foreach (var set in ReverseOrderSets.All)
+            {
+                RunTest(set);
+            }
+        }
+
+        [TestMethod]
+        public void RandomizedTests()
+        {
+            foreach (var set in RandomizedSets.All)
+            {
+                RunTest(set);
+            }
+        }
+    }
+}
--- tests/AlgoNet.Tests.Sorting/Sorting/MergeSortTests.cs
+++ tests/AlgoNet.Tests.Sorting/Sorting/MergeSortTests.cs
@@ -1,45 +1,17 @@
 ﻿// Adam Dernis © 2022
 
-using AlgoNet.Tests.Sorting.DataSets;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MES = AlgoNet.Sorting.MergeSort;
 
 namespace AlgoNet.Tests.Sorting.Sorting
 {
     [TestClass]
-    public class MergeSortTests
+    public class MergeSortTests : SortingTests
     {
-        private static void RunTest(int[] data)
+        protected override void RunTest(int[] data)
         {
             MES.Sort(data);
             Common.VerifySorted(data);
-        }
-
-        [TestMethod]
-        public void PresortedTests()
-        {
-            foreach (var set in PresortedSets.All)
-            {
-                RunTest(set);
-            }
-        }
-
-        [TestMethod]
-        public void ReverseOrderTests()
-        {
-            foreach (var set in ReverseOrderSets.All)
-            {
-                RunTest(set);
-            }
-        }
-
-        [TestMethod]
-        public void RandomizedTests()
-        {
-            foreach (var set in RandomizedSets.All)
-            {
-                RunTest(set);
-            }
         }
     }
 }
