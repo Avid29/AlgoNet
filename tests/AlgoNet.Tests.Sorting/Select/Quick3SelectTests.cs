b4a08831cf160a8f7affda2ee3a61b282f3a6c91
Adam Dernis <avid29@live.com>
2022-02-10 18:44:04 -0500

    Abstracted Selection tests

--- tests/AlgoNet.Tests.Sorting/Select/Quick3SelectTests.cs
+++ tests/AlgoNet.Tests.Sorting/Select/Quick3SelectTests.cs
@@ -2,16 +2,16 @@
 
 using AlgoNet.Tests.Sorting.Select.Abstract;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using QS = AlgoNet.Sorting.Select.QuickSelect;
+using QS3 = AlgoNet.Sorting.Select.Quick3Select;
 
 namespace AlgoNet.Tests.Sorting.Select
 {
     [TestClass]
-    public class QuickSelectTests : SelectionTests
+    public class Quick3SelectTests : SelectionTests
     {
         protected override void RunTest(int[] data, int k)
         {
-            int kth = QS.Select(data, k);
+            int kth = QS3.Select(data, k);
             Common.VerifyPartition(data, k, kth);
         }
     }
