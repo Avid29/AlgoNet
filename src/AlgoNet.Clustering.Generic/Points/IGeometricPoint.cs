70ff6a44d1c7e898704d90cfa32d418bd09693a9
Adam Dernis <avid29@live.com>
2022-03-03 11:24:48 -0500

    Generic Math Points

---
+++ src/AlgoNet.Clustering.Generic/Points/IGeometricPoint.cs
@@ -0,0 +1,34 @@
+﻿// Adam Dernis © 2021
+
+using System;
+
+namespace AlgoNet.Clustering.Generic
+{
+    /// <summary>
+    /// A Shape for geometric points in a cluster.
+    /// </summary>
+    /// <remarks>
+    /// Geometric points are defined by their absolute positions in space.
+    /// </remarks>
+    /// <typeparam name="T">The type being wrapped by the implementation.</typeparam>
+    /// <typeparam name="TFloat">The type of floating point used for distance.</typeparam>
+    /// <typeparam name="TWeight">The type of floating point used for weight.</typeparam>
+    public interface IGeometricPoint<T, TFloat, TWeight> : IMetricPoint<T, TFloat>
+        where TFloat : IFloatingPoint<TFloat>
+        where TWeight : INumber<TWeight>
+    {
+        /// <summary>
+        /// Gets the average value of a list of <typeparamref name="T"/> items.
+        /// </summary>
+        /// <param name="items">The list of points to average.</param>
+        /// <returns>The average of all items in the enumerable.</returns>
+        T Average(T[] items);
+
+        /// <summary>
+        /// Gets the weighted average value of a list of (T, TWeight) by point and weight.
+        /// </summary>
+        /// <param name="items">A weighted list of points.</param>
+        /// <returns>The weighted center of the points.</returns>
+        T WeightedAverage((T, TWeight)[] items);
+    }
+}
---
+++ src/AlgoNet.Clustering.Generic/Points/IMetricPoint.cs
@@ -0,0 +1,41 @@
+﻿// Adam Dernis © 2021
+
+using System;
+
+namespace AlgoNet.Clustering.Generic
+{
+    /// <summary>
+    /// A Shape for non-geometric points in a cluster.
+    /// </summary>
+    /// <remarks>
+    /// Metric points are defined by having relative distances from each other, but no absolute position in an space.
+    /// </remarks>
+    /// <typeparam name="T">The type being wrapped by the implementation.</typeparam>
+    /// <typeparam name="TDistance">The type of floating point used for distance.</typeparam>
+    public interface IMetricPoint<T, TDistance>
+        where TDistance : IFloatingPoint<TDistance>
+    {
+        /// <summary>
+        /// Gets the distance between <paramref name="it1"/> and <paramref name="it2"/>.
+        /// </summary>
+        /// <param name="it1">Point A.</param>
+        /// <param name="it2">Point B.</param>
+        /// <returns>The distance between point A and point B.</returns>
+        TDistance FindDistanceSquared(T it1, T it2);
+
+        /// <inheritdoc cref="AreEqual(T, T, TDistance)"/>
+        bool AreEqual(T it1, T it2);
+
+        /// <summary>
+        /// Checks equality of two items.
+        /// </summary>
+        /// <remarks>
+        /// For if there is an optimized way of comparing two points for equality.
+        /// </remarks>
+        /// <param name="it1">The item to compare.</param>
+        /// <param name="it2">The item to compare it to.</param>
+        /// <param name="error">The accepted error by distance for a comparison.</param>
+        /// <returns>Whether or not the items are equal.</returns>
+        bool AreEqual(T it1, T it2, TDistance error);
+    }
+}
