90e8da8161627bc1315c9b2e2055635dcf3117aa
Adam Dernis <avid29@live.com>
2022-03-03 11:48:53 -0500

    Generic Clustering IO

---
+++ src/AlgoNet.Clustering.Generic/IO/Cluster.cs
@@ -0,0 +1,16 @@
+﻿// Adam Dernis © 2021
+
+using System;
+
+namespace AlgoNet.Clustering.Generic
+{
+    /// <summary>
+    /// The base class for clusters.
+    /// </summary>
+    public abstract class Cluster<T, TShape, TDistance>
+        where T : unmanaged
+        where TShape : struct, IMetricPoint<T, TDistance>
+        where TDistance : IFloatingPoint<TDistance>
+    {
+    }
+}
