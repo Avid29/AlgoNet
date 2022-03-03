a01b539efd4ed2a9a8294cba89cc268b635a3ade
Adam Dernis <avid29@live.com>
2022-03-02 21:31:07 -0500

    Added Common operations using Generic Math from .NET 7 preview

---
+++ src/AlgoNet.Mathematics.Generic/Common/MinMax.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace AlgoNet.Mathematics.Generic
+{
+    internal class ExtraMath
+    {
+        /// <summary>
+        /// Returns the largest number.
+        /// </summary>
+        /// <param name="nums">The numbers to pull from.</param>
+        /// <returns>The largest number</returns>
+        public static T Max<T>(params T[] nums)
+            where T : INumber<T> => IterativeMax(nums.AsSpan())!;
+
+        /// <summary>
+        /// Returns the smallest number.
+        /// </summary>
+        /// <param name="nums">The numbers to pull from.</param>
+        /// <returns>The smallest number</returns>
+        public static T Min<T>(params T[] nums)
+            where T : INumber<T> => IterativeMin(nums.AsSpan());
+
+        internal static T IterativeMax<T>(Span<T> nums)
+            where T : INumber<T>
+        {
+            T max = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+                max = T.Max(max, nums[i]);
+            return max;
+        }
+
+        internal static T IterativeMin<T>(Span<T> nums)
+            where T : INumber<T>
+        {
+            T min = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+                min = T.Min(min, nums[i]);
+            return min;
+        }
+
+        internal static T RecursiveMax<T>(Span<T> nums)
+            where T : INumber<T>
+        {
+            if (nums.Length == 1) return nums[0];
+
+            int mid = nums.Length / 2;
+
+            T left = RecursiveMax(nums.Slice(0, mid));
+            T right = RecursiveMax(nums.Slice(mid));
+            return T.Max(left, right);
+        }
+
+        internal static T RecursiveMin<T>(Span<T> nums)
+            where T : INumber<T>
+        {
+            if (nums.Length == 1) return nums[0];
+
+            int mid = nums.Length / 2;
+
+            T left = RecursiveMin(nums.Slice(0, mid));
+            T right = RecursiveMin(nums.Slice(mid));
+            return T.Min(left, right);
+        }
+    }
+}
---
+++ src/AlgoNet.Mathematics.Generic/Common/Operations.cs
@@ -0,0 +1,36 @@
+﻿// Adam Dernis © 2022
+
+using System;
+
+namespace AlgoNet.Mathematics.Generic.Common
+{
+    public static class ExtraMath
+    {
+        public static T Sum<T>(params T[] values)
+            where T : unmanaged, IAdditionOperators<T, T, T>
+        {
+            T sum = values[0];
+            for (int i = 1; i < values.Length; i++)
+                sum += values[i];
+
+            return sum;
+        }
+
+        public static TBase Pow<TBase, TExponent>(TBase b, TExponent e)
+            where TBase : IMultiplyOperators<TBase, TBase, TBase>
+            where TExponent : IUnsignedNumber<TExponent>, IBinaryInteger<TExponent>
+        {
+            TExponent two = TExponent.One + TExponent.One;
+            return Pow(b, e, two);
+        }
+
+        private static TBase Pow<TBase, TExponent>(TBase b, TExponent e, TExponent two)
+            where TBase : IMultiplyOperators<TBase, TBase, TBase>
+            where TExponent : IUnsignedNumber<TExponent>, IBinaryInteger<TExponent>
+        {
+            if (e == two) return b * b;
+            if (e % two == TExponent.Zero) return Pow(b * b, e / two, two);
+            return Pow(b * b, e / two, two) * b;
+        }
+    }
+}
