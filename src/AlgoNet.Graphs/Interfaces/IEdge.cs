352bd828b4c042225f0e02b937bda9f2e62f21eb
Adam Dernis <avid29@live.com>
2022-03-18 13:32:54 -0400

    INode and IWeightedNode interfaces

--- src/AlgoNet.Graphs/Interfaces/IEdge.cs
+++
@@ -1,9 +0,0 @@
-﻿// Adam Dernis © 2022
-
-namespace AlgoNet.Graphs.Interfaces
-{
-    public interface IEdge
-    {
-        double Double { get; }
-    }
-}
---
+++ src/AlgoNet.Graphs/Nodes/INode.cs
@@ -0,0 +1,28 @@
+﻿// Adam Dernis © 2022
+
+using System.Collections.Generic;
+
+namespace AlgoNet.Graphs
+{
+    /// <summary>
+    /// A Shape for Nodes in a graph.
+    /// </summary>
+    /// <typeparam name="T">The type being wrapped by the implementation.</typeparam>
+    public interface INode<T>
+    {
+        /// <summary>
+        /// Checks if two nodes the distance between two nodes.
+        /// </summary>
+        /// <param name="node1">The source node.</param>
+        /// <param name="node2">The target node.</param>
+        /// <returns>True if the nodes share an edge.</returns>
+        bool CheckEdge(T node1, T node2);
+
+        /// <summary>
+        /// Gets a list of connected nodes to <paramref name="node"/>.
+        /// </summary>
+        /// <param name="node">The node to get the connected nodes to.</param>
+        /// <returns>Connected nodes</returns>
+        IEnumerable<T> GetConnectedNodes(T node);
+    }
+}
