bacd02ebc404e39ce504814225fd2e8ca7fa9c07
Adam Dernis <avid29@live.com>
2022-03-19 17:12:26 -0400

    Added Dijkstra's Algorithm

---
+++ src/AlgoNet.Graphs/Path/Dijkstras/Dijkstras.cs
@@ -0,0 +1,102 @@
+﻿// Adam Dernis © 2022
+
+using AlgoNet.Graphs.Path.Dijkstras;
+using System;
+using System.Collections.Generic;
+
+namespace AlgoNet.Graphs
+{
+    /// <summary>
+    /// A static class containing Dijkstra's algorithm methods.
+    /// </summary>
+    public static class Dijkstras
+    {
+        public static List<T>? Path<T, TShape>(T[] graph, T source, T? target, out Dictionary<T, double> dists, T? sentinel = default)
+            where T : IEquatable<T>
+            where TShape : struct, IWeightedNode<T> => Path<T, TShape>(graph, source, target, default, out dists);
+
+        public static List<T>? Path<T, TShape>(T[] graph, T source, T? target, TShape shape, out Dictionary<T, double> dists, T? sentinel = default)
+            where T : IEquatable<T>
+            where TShape : struct, IWeightedNode<T>
+        {
+            var context = new DijkstrasContext<T, TShape>(source, target, graph);
+
+            foreach (T node in graph)
+            {
+                context.Distances.Add(node, double.PositiveInfinity);
+                context.Previous.Add(node, sentinel);
+                context.Queue.Add(node); 
+            }
+
+            context.Distances[context.Source] = 0;
+
+            while (context.Queue.Count > 0)
+            {
+                T u = DequeMinDistance(context, out double uDistance);
+
+                if (u.Equals(context.Target))
+                    break;
+
+                foreach (T v in shape.GetConnectedNodes(u))
+                {
+                    double alt = uDistance + shape.GetEdgeLength(u, v);
+                    if (alt < context.Distances[v])
+                    {
+                        context.Distances[v] = alt;
+                        context.Previous[v] = u;
+                    }
+                }
+            }
+
+            dists = context.Distances;
+            return BackTracePath(context, sentinel);
+        }
+
+        private static List<T>? BackTracePath<T, TShape>(DijkstrasContext<T, TShape> context, T? sentinel)
+            where T : IEquatable<T>
+            where TShape : struct, IWeightedNode<T>
+        {
+            // If there's no target return a null path
+            if (context.Target == null) return null;
+
+            List<T> path = new List<T>();
+            T? u = context.Target;
+
+            while (context.Previous.TryGetValue(u, out T? prev))
+            {
+                if (prev == null || prev.Equals(sentinel)) break;
+
+                path.Add(u);
+                u = context.Previous[u];
+            }
+
+            if (context.Source.Equals(u))
+            {
+                path.Reverse();
+                return path;
+            }
+            return null;
+        }
+
+        private static T DequeMinDistance<T, TShape>(DijkstrasContext<T, TShape> context, out double distance)
+            where T : IEquatable<T>
+            where TShape : struct, IWeightedNode<T>
+        {
+            int minIndex = 0;
+            T minNode = context.Queue[minIndex];
+            distance = context.Distances[minNode];
+            for (int i = 0; i < context.Queue.Count; i++)
+            {
+                T node = context.Queue[i];
+                if (context.Distances[node] < distance)
+                {
+                    minIndex = i;
+                    minNode = node;
+                    distance = context.Distances[node];
+                }
+            }
+            context.Queue.RemoveAt(minIndex);
+            return minNode;
+        }
+    }
+}
