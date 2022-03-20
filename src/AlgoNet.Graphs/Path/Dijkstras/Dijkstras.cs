cb6e002c3e8370a860d360bfa10301d21201cd9c
Adam Dernis <avid29@live.com>
2022-03-20 14:19:07 -0400

    Fixed Dijkstra's path output to include source

--- src/AlgoNet.Graphs/Path/Dijkstras/Dijkstras.cs
+++ src/AlgoNet.Graphs/Path/Dijkstras/Dijkstras.cs
@@ -72,6 +72,7 @@ namespace AlgoNet.Graphs
 
             if (context.Source.Equals(u))
             {
+                path.Add(u);
                 path.Reverse();
                 return path;
             }
