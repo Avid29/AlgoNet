39e306d04ed84593feaee2c1eb273250f8ef03e4
Adam Dernis <avid29@live.com>
2022-04-26 18:46:04 -0400

    Implemented heap sort

--- src/AlgoNet.Sorting/Sorting/Internal/HeapSort.cs
+++ src/AlgoNet.Sorting/Sorting/Internal/HeapSort.cs
@@ -1,6 +1,10 @@
 ﻿// Adam Dernis © 2022
 
-namespace AlgoNet.Sorting.Sorting.Internal
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace AlgoNet.Sorting
 {
     /// Heap sort works by converting the array into a heap, then reading it back into an array.
     /// This can be done in place by heapifying the array from the front, then reading into the back
@@ -67,17 +71,12 @@ namespace AlgoNet.Sorting.Sorting.Internal
     ///         3(1)         2(2)
     ///      1(3)
     /// 
-    /// Step 10: (9 skipped)
-    ///     Group | Heap  | - Sorted - - -| 
-    ///     Value | 2   1 | 3   4   5   6 |
-    ///     Index | 0   1 | 2   3   4   5 |
-    /// 
-    ///     Tree:
-    ///             2(0)
-    ///         1(1)
+    /// Step 11: (9,10,11 skipped)
+    ///     Group | - Sorted - - - - - -  | 
+    ///     Value | 1 | 2   3   4   5   6 |
+    ///     Index | 0 | 1   2   3   4   5 |
     ///         
-    ///     At this step we have two items left in a max heap, so we guarentee it is reverse sorted.
-    ///     Just swap the two values.
+    ///     At this step we have just one item left in the heap, so we can leave it and call the entire space sorted.
 
 
     /// <summary>
@@ -85,6 +84,95 @@ namespace AlgoNet.Sorting.Sorting.Internal
     /// </summary>
     internal static class HeapSort
     {
+        /// <inheritdoc cref="Sort{T}(Span{T})"/>
+        internal static void Sort<T>(T[] array) where T : IComparable<T>
+            => Sort(array.AsSpan());
 
+
+#if NET5_0_OR_GREATER
+        internal static void Sort<T>(List<T> list) where T : IComparable<T>
+            => Sort(CollectionsMarshal.AsSpan(list));
+#endif
+
+        /// <summary>
+        /// Runs heap sort on an array.
+        /// </summary>
+        /// <typeparam name="T">The type of item in the array being sorted.</typeparam>
+        /// <param name="array">The array to sort.</param>
+        internal static void Sort<T>(Span<T> array)
+            where T : IComparable<T>
+        {
+            // Heapify
+            for (int i = 2; i <= array.Length; i++)
+            {
+                HeapifyUp(array.Slice(0, i));
+            }
+            
+            // Unheapify
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                // Pop the top, then heapify down
+                Common.Swap(ref array[i], ref array[0]);
+                HeapifyDown(array.Slice(0, i));
+            }
+        }
+
+        private static void HeapifyUp<T>(Span<T> array)
+            where T : IComparable<T>
+        {
+            // Start at the last element
+            int n = array.Length - 1;
+
+            // Stop if the value has no parent
+            while (n != 0)
+            {
+                int parent = ((n + 1) / 2) - 1;
+
+                // If the value is smaller than its parent, the array is a heap
+                if (array[n].CompareTo(array[parent]) <= 0)
+                {
+                    return;
+                }
+
+                // Swap the value with its parent
+                Common.Swap(ref array[n], ref array[parent]);
+                n = parent;
+            }
+        }
+
+        private static void HeapifyDown<T>(Span<T> array)
+            where T : IComparable<T>
+        {
+            int n = 0;
+
+            while (true)
+            {
+                int right = (n + 1) * 2;
+                int left = right - 1;
+
+                // The value has no children
+                if (left >= array.Length)
+                {
+                    return;
+                }
+
+                // Find the larger of the children and track its index
+                int larger = left;
+                if (right < array.Length && array[right].CompareTo(array[left]) >= 0)
+                {
+                    larger = right;
+                }
+
+                // If the value is larger than its larger child, the array is a heap
+                if (array[n].CompareTo(array[larger]) >= 0)
+                {
+                    return;
+                }
+
+                // Swap the value with its largest child
+                Common.Swap(ref array[n], ref array[larger]);
+                n = larger;
+            }
+        }
     }
 }
