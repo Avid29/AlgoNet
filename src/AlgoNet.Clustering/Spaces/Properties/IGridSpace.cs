2971e97e8e7c46b8c17f040e34192ba526d50aeb
Adam Dernis <avid29@live.com>
2022-05-03 16:40:07 -0400

    Added docs for IGridSpace

--- src/AlgoNet.Clustering/Spaces/Properties/IGridSpace.cs
+++ src/AlgoNet.Clustering/Spaces/Properties/IGridSpace.cs
@@ -2,8 +2,19 @@
 
 namespace AlgoNet.Clustering
 {
+    /// <summary>
+    /// An <see cref="ISpace{T}"/> where points can be split into a grid.
+    /// </summary>
+    /// <typeparam name="T">The type of points in the space.</typeparam>
+    /// <typeparam name="TCell">A type identifying the cell in the grid.</typeparam>
     public interface IGridSpace<T, TCell> : ISpace<T>
     {
+        /// <summary>
+        /// Gets the cell of a point in the space.
+        /// </summary>
+        /// <param name="value">The point.</param>
+        /// <param name="detail">The level of detail for cells.</param>
+        /// <returns>The cell of the point in the space.</returns>
         TCell GetCell(T value, double detail);
     }
 }
